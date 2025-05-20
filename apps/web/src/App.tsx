744dc91e17bb8fabe0ce66d10464d164531d551e
Jaron <jaron@ed.link>
2025-05-19 23:35:45 -0500

    browser routing working

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,20 +1,63 @@
+import { BrowserRouter, Router, Routes, Route, Navigate, useLocation } from 'react-router';
 import { WorldProvider } from './context/world-context';
 import MystwrightSidebar from './components/Sidebar';
 import MainPage from './components/MainPage';
 import './App.css';
+import { useEffect } from 'react';
 
-function App() {
+function AppRoutes() {
+    // const location = useLocation();
+
+    // Save the current path when it changes
+    // useEffect(() => {
+    //     if (location.pathname.startsWith('/app')) {
+    //         localStorage.setItem(LAST_PATH_KEY, location.pathname);
+    //     }
+    // }, [location]);
 
     return (
-        <>
-            <WorldProvider>
-                <div className='mystwright-app dark'>
-                    <MystwrightSidebar />
+        <Routes>
+            <Route path="/" element={
+                <Layout sidebar>
+                    <AppIntro />
+                </Layout>
+            } />
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/signup" element={<Signup />} /> */}
+            <Route path="/app" element={
+                <Layout sidebar>
                     <MainPage />
-                </div>
-            </WorldProvider>
-        </>
+                </Layout>
+            } />
+            {/* Add a catch-all route */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
     );
+}
+
+function Layout({ sidebar, children }: { sidebar?: boolean, children: React.ReactNode }) {
+    return (
+        <WorldProvider>
+            {sidebar && <MystwrightSidebar />}
+            {children}
+        </WorldProvider>
+    );
+}
+
+function AppIntro() {
+    return (
+        <MainPage />
+    );
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="mystwright-app dark">
+                <AppRoutes />
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App;
